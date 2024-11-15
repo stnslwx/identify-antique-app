693a79a6d32c42705046ebb89391bf885d6b6f2b
Stanislaw <stanislaw.ios.dev@gmail.com>
2024-11-15 14:19:41 +0300

    project init

---
+++ identify-antique/identify-antique/identify_antiqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  identify_antiqueApp.swift
+//  identify-antique
+//
+//  Created by Стас Асташенко on 15/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct identify_antiqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
